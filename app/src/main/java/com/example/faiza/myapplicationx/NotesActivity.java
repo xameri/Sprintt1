604c5dcf628faa51ededf34c1384ab3c0b049aca
xameri <afarah1@css.edu>
2018-05-09 19:33:03 -0500

    // I am here commiting Sprint 1 for Final Project

--- app/src/main/java/com/example/faiza/myapplicationx/NotesActivity.java
+++ app/src/main/java/com/example/faiza/myapplicationx/NotesActivity.java
@@ -1,5 +1,5 @@
 package com.example.faiza.myapplicationx;
-
+// I am here commiting Sprint 1
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
